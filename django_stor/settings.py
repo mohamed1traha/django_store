ab2abf6f2f5a1b081fab5b856a060f928295a197
mohamed <ttmm187110@gmail.com>
2025-02-04 06:03:44 +0300

    Final touches

--- django_stor/settings.py
+++ django_stor/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_*%nop=w)1qxs!fif&sxq()3swh)$$pj1bxq_2tzg$092-fbsh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'hsoub-store.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -45,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
